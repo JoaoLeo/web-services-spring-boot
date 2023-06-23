b460776b88278886402b2f1ea91ed3dcfdce5e59
João Leonardo <joaoleonardo9921@gmail.com>
2023-06-23 14:27:14 -0300

    limpando codigo

--- src/main/java/com/curso/repositories/ItemPedidoRepository.java
+++ src/main/java/com/curso/repositories/ItemPedidoRepository.java
@@ -1,7 +1,6 @@
 package com.curso.repositories;
 
 import com.curso.entities.ItemPedido;
-import com.curso.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
